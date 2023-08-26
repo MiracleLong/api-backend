84b86b92c866b309ddf6e836418ed8cf4b782cb4
Miracle <2893496202@qq.com>
2023-08-26 14:17:05 +0800

    补充

--- src/main/java/com/hui/api/service/impl/InterfaceInfoServiceImpl.java
+++ src/main/java/com/hui/api/service/impl/InterfaceInfoServiceImpl.java
@@ -3,23 +3,20 @@ package com.hui.api.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hui.api.common.ErrorCode;
 import com.hui.api.exception.BusinessException;
-import com.hui.api.exception.ThrowUtils;
-import com.hui.api.model.entity.InterfaceInfo;
+import com.hui.api.mapper.InterfaceInfoMapper;
 import com.hui.api.model.entity.InterfaceInfo;
 import com.hui.api.service.InterfaceInfoService;
-import com.hui.api.mapper.InterfaceInfoMapper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 /**
-* @author 86176
+* @author dargon
 * @description 针对表【interface_info(接口信息表)】的数据库操作Service实现
 * @createDate 2023-08-04 20:57:54
 */
 @Service
 public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, InterfaceInfo>
     implements InterfaceInfoService{
-
     @Override
     public void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add) {
         if (interfaceInfo == null) {
@@ -30,6 +27,7 @@ public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, I
         /*if (add) {
             ThrowUtils.throwIf(StringUtils.isAnyBlank(title, content, tags), ErrorCode.PARAMS_ERROR);
         }*/
+
         // 有参数则校验
         if (StringUtils.isNotBlank(name)) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
